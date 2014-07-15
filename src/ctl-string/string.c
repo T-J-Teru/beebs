14eaf7f407c22b47147ebccc2618a8bb5c97b7df
Andrew Burgess <andrew.burgess@embecosm.com>
2014-07-15 11:26:00 +0100

    Remove unused variable.
    Continue to get benchmarks building without any build warnings.
    
    ChangeLog:
    
    	* src/ctl-string/string.c (ctl_StringSetSubStr): Remove unused
    	variable.

--- src/ctl-string/string.c
+++ src/ctl-string/string.c
@@ -280,7 +280,6 @@ int ctl_StringSetSubStr(ctl_string* s, size_t begin, size_t end, char* string)
   size_t len2=strlen(string);
   size_t len1=end-begin;
   size_t diff=len2-len1;
-  int a=0;
   CTL_RANGE(end>=s->size)
   {
     ctl_errno=CTL_OUT_OF_RANGE;
